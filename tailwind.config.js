3f655b560f573d3b99711a344bf701ca357968dd
aniket.bose <boseaniket48@gmail.com>
2024-08-22 02:00:10 +0530

    Few more updations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      customGray: 'rgb(0, 245, 245)',
+      boxShadow: {
+        'custom-light': '0 0px 5px rgba(219, 219, 219)',
+        'custom-heavy': '0 0px 15px rgba(0, 0, 0)'
+      },
+    },
   },
   plugins: [],
 }
-
-
